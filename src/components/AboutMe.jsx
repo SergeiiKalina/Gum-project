cdbf4b4f51c04438a2f6aedbcd871d783cba8a61
Sergeii Kalyna <setserg02@gmail.com>
2023-06-18 10:38:08 +0300

    add scss

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,21 +1,18 @@
 import img from '../image/IMG-39ecee3d968d544e854d0d857e831f16-V-removebg-preview.png'
-import img2 from '../image/2.jpg'
 import style from './aboutme.module.css'
+import stylescss from './aboutme.module.scss'
 
 function AboutMe() {
     return (
         <div className={style.block}>
             <div className={style.blockImg}>
                 <img src={img} className={style.mainFoto} />
-                <img src={img2} className={style.hiddenImg} />
             </div>
             <div className={style.block_paragraph}>
-                <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Expedita repellendus, necessitatibus maxime sed molestias a
-                    vero accusamus. Similique, officia. Quaerat facilis vel
-                    architecto assumenda, tenetur et laborum molestias
-                    distinctio facere!
+                <p className={stylescss.paragraphe}>
+                    <span>Lorem</span>, ipsum dolor sit amet consectetur
+                    adipisicing elit. Expedita repellendus, necessitatibus
+                    maxime sed molestias a vero accusamus.
                 </p>
             </div>
         </div>
