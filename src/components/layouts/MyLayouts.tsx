45fce508504c83aed1e0d5e3fef6c3a1ed3c1d6c
Sergeii Kalyna <setserg02@gmail.com>
2024-01-11 11:23:09 +0200

    delete footer

--- src/components/layouts/MyLayouts.tsx
+++ src/components/layouts/MyLayouts.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom"
 import Header from "../header/Header"
 import React from "react"
-import Footer from "../Footer/Footer"
 
 function MyLayouts(): React.JSX.Element {
     return (
         <>
             <Header />
             <Outlet />
-            <Footer />
         </>
     )
 }
